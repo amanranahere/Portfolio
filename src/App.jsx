c2723b02f803d14541cb932bf52d58c631542902
Aman Rana <amanranahere@gmail.com>
2024-11-10 00:50:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NumLoader from "./components/Loader/NumLoader.jsx";
+
+function App() {
+  return (
+    <>
+      <NumLoader />
+    </>
+  );
+}
+
+export default App;
