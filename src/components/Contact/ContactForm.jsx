59500e16fcfd27362794d838154a338f5dadddd8
Aman Rana <amanranahere@gmail.com>
2025-02-26 12:00:02 +0530

    add css to contact page

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import emailjs from "@emailjs/browser";
+import { SiLinkedin, SiGithub, SiX } from "react-icons/si";
 
 export default function ContactForm() {
   const [formData, setFormData] = useState({
@@ -44,91 +45,131 @@ export default function ContactForm() {
     <div className="contact-form h-full flex justify-center items-center">
       <form
         onSubmit={handleSubmit}
-        className="w-full h-full flex flex-col border-x border-b border-dotted flex-grow"
+        className="w-full h-full flex flex-col border-x border-dotted flex-grow"
       >
         <div className="flex flex-col md:flex-row">
-          <input
-            type="text"
-            name="name"
-            placeholder="NAME"
-            value={formData.name}
-            onChange={handleChange}
-            required
-            className="w-full p-6 text-4xl leading-5 bg-[#343434] text-[#f7f7f7] outline-none border-b md:border-r border-dotted border-[#f7f7f7] transition-colors duration-300"
-          />
+          <div className="w-full relative">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+              className="w-full p-6 text-4xl leading-5 bg-[#343434] hover:bg-[#3f3f3f] focus:bg-[#3f3f3f] text-[#f7f7f7] outline-none border-b md:border-r border-dotted border-[#f7f7f7] transition-colors duration-500"
+            />
+            <span className="absolute top-3 right-3 text-sm text-gray-400">
+              *Required
+            </span>
+          </div>
 
-          <input
-            type="email"
-            name="email"
-            placeholder="EMAIL"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            className="w-full p-6 text-4xl leading-5 bg-[#343434] text-[#f7f7f7] outline-none border-b border-l lg:border-l-0  border-dotted border-[#f7f7f7] focus:border-[#f7f7f7] transition duration-300"
-          />
+          <div className="w-full relative">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="w-full p-6 text-4xl leading-5 bg-[#343434] hover:bg-[#3f3f3f] focus:bg-[#3f3f3f] text-[#f7f7f7] outline-none border-b border-l lg:border-l-0  border-dotted border-[#f7f7f7] focus:border-[#f7f7f7] transition duration-500"
+            />
+            <span className="absolute top-3 right-3 text-sm text-gray-400">
+              *Required
+            </span>
+          </div>
         </div>
 
         <div className="w-full flex flex-col md:flex-row">
-          <input
-            type="text"
-            name="company"
-            placeholder="COMPANY"
-            value={formData.company}
-            onChange={handleChange}
-            className="w-full p-6 text-4xl leading-5 bg-[#343434] text-[#f7f7f7] outline-none border-b md:border-r border-dotted border-[#f7f7f7] focus:border-[#f7f7f7] transition duration-300"
-          />
+          <div className="w-full relative">
+            <input
+              type="text"
+              name="company"
+              placeholder="COMPANY"
+              value={formData.company}
+              onChange={handleChange}
+              className="w-full p-6 text-4xl leading-5 bg-[#343434] hover:bg-[#3f3f3f] focus:bg-[#3f3f3f] text-[#f7f7f7] outline-none border-b md:border-r border-dotted border-[#f7f7f7] focus:border-[#f7f7f7] transition duration-500"
+            />
+            <span className="absolute top-3 right-3 text-sm text-gray-400">
+              Optional
+            </span>
+          </div>
 
-          <input
-            type="tel"
-            name="phone"
-            placeholder="PHONE NUMBER"
-            value={formData.phone}
-            onChange={handleChange}
-            className="w-full p-6 text-4xl leading-5 bg-[#343434] text-[#f7f7f7] outline-none md:border-b border-dotted border-[#f7f7f7] focus:border-[#f7f7f7] transition duration-300"
-          />
+          <div className="w-full relative">
+            <input
+              type="tel"
+              name="phone"
+              placeholder="PHONE NUMBER"
+              value={formData.phone}
+              onChange={handleChange}
+              className="w-full p-6 text-4xl leading-5 bg-[#343434] hover:bg-[#3f3f3f] focus:bg-[#3f3f3f] text-[#f7f7f7] outline-none md:border-b border-dotted border-[#f7f7f7] focus:border-[#f7f7f7] transition duration-500"
+            />
+            <span className="absolute top-3 right-3 text-sm text-gray-400">
+              Optional
+            </span>
+          </div>
         </div>
 
-        <textarea
-          name="message"
-          placeholder="MESSAGE"
-          value={formData.message}
-          onChange={handleChange}
-          required
-          rows="2"
-          className="flex-grow h-full p-6 text-4xl leading-5 bg-[#343434] text-[#f7f7f7] outline-none border-dotted border-[#f7f7f7] transition-colors duration-300 resize-none"
-        ></textarea>
+        <div className="w-full h-full relative">
+          <textarea
+            name="message"
+            placeholder="MESSAGE"
+            value={formData.message}
+            onChange={handleChange}
+            required
+            rows="2"
+            className="flex-grow w-full h-full p-6 text-4xl bg-[#343434] hover:bg-[#3f3f3f] focus:bg-[#3f3f3f] text-[#f7f7f7] outline-none border-dotted border-[#f7f7f7] transition-colors duration-500 resize-none"
+          ></textarea>
+          <span className="absolute top-3 right-3 text-sm text-gray-400">
+            *Required
+          </span>
+        </div>
 
         <div className="w-full flex flex-col md:flex-row-reverse text-4xl leading-5">
           <button
             type="submit"
-            className="w-[40%] flex justify-center items-center bg-[#e7e7e7] hover:bg-[#d0d0d0] active:bg-[#c4c4c4]"
+            className="w-[40%] flex justify-center items-center bg-gray-800 text-white 
+               hover:bg-gray-700 active:bg-gray-600 relative button-glow"
+            style={{ animationDelay: "1.2s" }}
           >
-            SEND
+            <span> SEND </span>
+            <div className="absolute -top-8 text-lg text-white">
+              {status && <p>{status}</p>}
+            </div>
           </button>
 
-          <div className="w-[60%] p-3 bg-[#444444] flex justify-evenly">
+          <div className="w-[60%] flex justify-evenly">
             <a
               href=""
-              className="hover:text-gray-600 font-medium transition duration-300"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-full h-full p-6 bg-gray-300 text-white hover:bg-gray-200 transition duration-500 flex justify-center items-center gap-2 button-glow"
+              style={{ animationDelay: "0.3s" }}
             >
-              LINKEDN
+              <SiLinkedin /> <span>LINKEDIN</span>
             </a>
+
             <a
-              href=""
-              className="hover:text-gray-600 font-medium transition duration-300"
+              href="https://github.com/amanranahere"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-full h-full p-6 bg-gray-500 text-white hover:bg-gray-400 transition duration-500 flex justify-center items-center gap-2 button-glow"
+              style={{ animationDelay: "0.6s" }}
             >
-              GITHUB
+              <SiGithub /> <span>GITHUB</span>
             </a>
+
             <a
               href=""
-              className="hover:text-gray-600 font-medium transition duration-300"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-full h-full p-6 bg-gray-700 text-white hover:bg-gray-600 transition duration-500 flex justify-center items-center gap-2 button-glow"
+              style={{ animationDelay: "0.9s" }}
             >
-              TWITTER
+              <SiX />
             </a>
           </div>
         </div>
       </form>
-      {status && <p>{status}</p>}
     </div>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,18 +6,18 @@ function Contact() {
   return (
     <div className="pt-24 lg:pt-0 bg-[#343434] h-screen flex flex-col">
       <div className="flex flex-col lg:flex-row">
-        <div className="p-8 pt-10 bg-[#e7e7e7] text-[4rem] md:text-[15vw] leading-[0.9] tracking-tight font-medium whitespace-nowrap flex justify-center items-center">
+        <div className="p-8 pt-10 bg-gray-200 text-[4rem] md:text-[15vw] leading-[0.9] tracking-tight font-medium whitespace-nowrap flex justify-center items-center select-none">
           Let's Talk
         </div>
 
         <div className="bg-[#f7f7f7] flex flex-col justify-between p-4 gap-10 lg:gap-0">
-          <div className="text-3xl md:pr-32 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            illum ratione perferendis sed ab odio dolor nesciunt totam id.
+          <div className="text-3xl md:pr-32 select-none">
+            Got a project in mind or just want to say hi? Let’s create something
+            amazing together. Feel free to reach out!
           </div>
 
           <div className="flex justify-between text-sm">
-            <span className="leading-none">Prefer Email?</span>
+            <span className="leading-none select-none">Prefer Email?</span>
             <a
               href="mailto:amanranahere@gmail.com"
               className="underline leading-none"
