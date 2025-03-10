797777c0fa46cf01dd2bba056e9ae2562b382f40
Aman Rana <amanranahere@gmail.com>
2025-03-11 01:44:02 +0530

    add animated light/dark button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import DarkModeToggle from "../DarkModeToggle";
 
 function Footer() {
   return (
-    <div className="fixed bottom-0 -z-10 p-6 bg-[#1e1919] flex flex-col justify-center items-center">
+    <div className="fixed bottom-0 -z-10 p-6 bg-[#1e1919] flex justify-center items-center">
       <div className="p-6 h-80 lg:p-20 text-[#f1f1f1] w-full text-xl text-center">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
         quibusdam, necessitatibus nemo ipsam sapiente recusandae magni
