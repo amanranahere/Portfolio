e47afa4b78edd3f3a45a01e1c6b42f322d8d597c
Aman Rana <amanranahere@gmail.com>
2025-03-21 01:37:17 +0530

    replace the project images

--- src/components/Home/WorkSection.jsx
+++ src/components/Home/WorkSection.jsx
@@ -1,11 +1,17 @@
 import { useRef } from "react";
 import { motion, useScroll, useTransform, useSpring } from "framer-motion";
+import brickBreaker from "../../assets/images/brickBreaker.png";
+import currencyConverter from "../../assets/images/currencyConverter.png";
+import flappyBird from "../../assets/images/flappyBird.png";
+import snakeGame from "../../assets/images/snakeGame.png";
+import vidron from "../../assets/images/vidronHome.png";
+import weblog from "../..//assets/images/weblog.png";
 
 function WorkSection() {
   const containerRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: containerRef,
-    offset: ["start start", "end start"],
+    offset: ["start start", "end end"],
   });
 
   const smoothScroll = useSpring(scrollYProgress, {
@@ -16,7 +22,7 @@ function WorkSection() {
   const firstLineTransformX = useTransform(
     smoothScroll,
     [0, 1],
-    ["16%", "-60%"]
+    ["23%", "-25%"]
   );
 
   const projectsTransformX = useTransform(
@@ -28,47 +34,61 @@ function WorkSection() {
   const secondLineTransformX = useTransform(
     smoothScroll,
     [0, 1],
-    ["0%", "60%"]
+    ["-26%", "25%"]
   );
 
   return (
-    <section ref={containerRef} className="relative h-[500vh] bg-[#1e1e1e]">
+    <section ref={containerRef} className="relative h-[900vh] bg-[#1e1e1e]">
       <div className="sticky top-0 flex flex-col h-screen justify-center items-center overflow-hidden">
         <motion.div
           style={{ x: firstLineTransformX }}
           className=" text-[14rem] leading-[0.7em] tracking-tighter font-extrabold text-[#f3f1ec] whitespace-nowrap uppercase select-none oswald-text"
         >
-          EXPLORE MY WORK
+          Explore the Depth of My Work
         </motion.div>
 
         <motion.div
           style={{ x: projectsTransformX }}
           className="flex gap-10 z-10"
         >
-          <div className="w-[30vw]  aspect-video flex-shrink-0 bg-gray-800 rounded-xl p-6">
-            <div className="text-white text-3xl mb-4 flex justify-center items-center">
-              Project One
-            </div>
+          <div className="w-[30vw] aspect-video flex-shrink-0 bg-gray-800 rounded-xl">
+            <img
+              className="w-full h-full rounded-2xl"
+              src={currencyConverter}
+              alt="Currency Converter"
+            />
           </div>
 
-          <div className="w-[30vw]  aspect-video flex-shrink-0 bg-gray-700 rounded-xl p-6">
-            <div className="text-white text-3xl mb-4">Project Two</div>
+          <div className="w-[30vw] aspect-video flex-shrink-0 bg-gray-700 rounded-xl">
+            <img
+              className="w-full h-full rounded-2xl"
+              src={weblog}
+              alt="Weblog"
+            />
           </div>
 
-          <div className="w-[30vw]  aspect-video flex-shrink-0 bg-gray-600 rounded-xl p-6">
-            <div className="text-white text-3xl mb-4">Project Three</div>
+          <div className="w-[30vw] aspect-video flex-shrink-0 bg-gray-600 rounded-xl">
+            <img
+              className="w-full h-full rounded-2xl"
+              src={vidron}
+              alt="Vidron"
+            />
           </div>
 
-          <div className="w-[30vw]  aspect-video flex-shrink-0 bg-gray-800 rounded-xl p-6">
-            <div className="text-white text-3xl mb-4 flex justify-center items-center">
-              Project Four
-            </div>
+          <div className="w-[30vw] aspect-video flex-shrink-0 bg-gray-800 rounded-xl">
+            <img
+              className="w-full h-full rounded-2xl"
+              src={brickBreaker}
+              alt="Brick Breaker"
+            />
           </div>
 
-          <div className="w-[30vw]  aspect-video flex-shrink-0 bg-gray-800 rounded-xl p-6">
-            <div className="text-white text-3xl mb-4 flex justify-center items-center">
-              Project Five
-            </div>
+          <div className="w-[30vw] aspect-video flex-shrink-0 bg-gray-800 rounded-xl">
+            <img
+              className="w-full h-full rounded-2xl"
+              src={snakeGame}
+              alt="Snake Game"
+            />
           </div>
         </motion.div>
 
@@ -76,7 +96,7 @@ function WorkSection() {
           style={{ x: secondLineTransformX }}
           className="z-20 text-[14rem] leading-[0.5em] tracking-tighter font-extrabold text-[#f3f1ec] whitespace-nowrap uppercase select-none oswald-text"
         >
-          These projects are made by me over the years
+          Built with Expertise & Creativity
         </motion.div>
       </div>
     </section>
